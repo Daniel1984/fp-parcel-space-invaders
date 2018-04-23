09a49335079277985530abbbf5ef582bc7b973f6
daniel1984 <aciddaniel@gmail.com>
2018-04-23 09:58:22 +0200

    adding hero ship

--- src/index.js
+++ src/index.js
@@ -11,14 +11,28 @@ function paintStars(stars) {
   ctx.fillStyle = '#000000';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = '#ffffff';
+  ctx.save();
 
   stars.forEach(({ x, y, size }) => {
     ctx.fillRect(x, y, size, size);
   });
+
+  ctx.restore();
+}
+
+function drawShip(x, y, width, color, direction) {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(x - width, y);
+  ctx.lineTo(x, direction === 'up' ? y - width : y + width);
+  ctx.lineTo(x + width, y);
+  ctx.lineTo(x - width, y);
+  ctx.fill();
 }
 
 function renderScene({ stars, hero }) {
   paintStars(stars);
+  drawShip(hero.x, hero.y, 20, '#ff00ff', 'up');
 }
 
 Game(canvas).subscribe(renderScene);
